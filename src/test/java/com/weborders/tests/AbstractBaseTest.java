808f4565f818727c988b78f2930be74d0e8f04e7
Adil Doshayev <doshayev@gmail.com>
2020-04-05 14:28:36 -0500

    update

--- src/test/java/com/weborders/tests/AbstractBaseTest.java
+++ src/test/java/com/weborders/tests/AbstractBaseTest.java
@@ -1,16 +1,33 @@
 package com.weborders.tests;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.weborders.utilities.BrowserUtilities;
 import com.weborders.utilities.ConfigurationReader;
 import com.weborders.utilities.Driver;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 
 public class AbstractBaseTest {
     protected WebDriver driver = Driver.getDriver();
+
+    protected static ExtentReports extentReports;
+    protected static ExtentHtmlReporter extentHtmlReporter;
+    protected static ExtentTest extentTest;
+
+    @BeforeTest
+    public void beforeTest(){
+        extentReports = new ExtentReports();
+    }
+    @AfterTest
+    public void afterTest(){
+        extentReports.flush();
+    }
 
     @BeforeMethod
     public void setup(){
@@ -23,6 +40,7 @@ public class AbstractBaseTest {
         if (testResult.getStatus() == ITestResult.FAILURE){
             String screenshotLocation = BrowserUtilities.getScreenshot(testResult.getName());
         }
+        BrowserUtilities.wait(3);
         Driver.closeDriver();
     }
 }

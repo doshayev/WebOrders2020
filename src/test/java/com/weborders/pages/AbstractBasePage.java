1be4159c2207369f6d7d638b18f9ba7b36eaaed5
Adil Doshayev <doshayev@gmail.com>
2020-04-05 15:25:59 -0500

    update

--- src/test/java/com/weborders/pages/AbstractBasePage.java
+++ src/test/java/com/weborders/pages/AbstractBasePage.java
@@ -1,10 +1,12 @@
 package com.weborders.pages;
 
 import com.weborders.utilities.Driver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 //it meant to be extended
@@ -21,8 +23,11 @@ public abstract class AbstractBasePage {
 
     public AbstractBasePage(){
         PageFactory.initElements(Driver.getDriver(),this);
+    }
 
-
+    public void navigateTo(String component){
+        String locator = "//a[text()='"+component+"']";
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(locator))).click();
 
     }
 

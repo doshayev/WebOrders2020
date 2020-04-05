7fb36c3fdabd554f2e18c66b9bfc597a5df89fdd
Adil Doshayev <doshayev@gmail.com>
2020-04-05 15:23:12 -0500

    update

--- src/test/java/com/weborders/pages/AbstractBasePage.java
+++ src/test/java/com/weborders/pages/AbstractBasePage.java
@@ -2,6 +2,8 @@ package com.weborders.pages;
 
 import com.weborders.utilities.Driver;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -9,6 +11,13 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public abstract class AbstractBasePage {
     protected WebDriver driver = Driver.getDriver();
     protected WebDriverWait wait = new WebDriverWait(driver,20);
+
+    @FindBy(tagName = "h1")
+    protected WebElement pageLogo;
+
+    public String getPageLogoText(){
+        return pageLogo.getText();
+    }
 
     public AbstractBasePage(){
         PageFactory.initElements(Driver.getDriver(),this);
